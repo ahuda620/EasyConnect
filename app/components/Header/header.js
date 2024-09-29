31757f60f78bacced5cdffed6bf0def765f4676b
ahuda620 <ahuda620@gmail.com>
2024-09-29 00:19:27 -0700

    added profile dropdown menu

--- app/components/Header/header.js
+++ app/components/Header/header.js
@@ -1,6 +1,20 @@
+"use client";
+
 import styles from "./header.module.css";
+import Link from "next/link";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faFileLines,
+  faBookmark,
+  faRightFromBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
+  const { user, error, isLoading } = useUser();
+  // console.log(user);
+
   return (
     <header className={styles.container}>
       <img
@@ -10,7 +24,7 @@ export default function Header() {
       ></img>
       <ul className={styles.centerNavLinks}>
         <li>
-          <a href="#">Find a Job</a>
+          <a href="../../test">Test</a>
         </li>
         <li>
           <a href="#">Placeholder</a>
@@ -22,14 +36,46 @@ export default function Header() {
           <a href="#">Placeholder</a>
         </li>
       </ul>
-      <ul className={styles.rightNavLinks}>
-        <li>
-          <a href="#">Login</a>
-        </li>
-        <li>
-          <a href="#">Sign Up</a>
-        </li>
-      </ul>
+      {user ? (
+        <div className={styles.userProfileContainer}>
+          <div className={styles.userLoggedIn}>
+            <p>Welcome, {user.given_name}!</p>
+            <img src={user.picture}></img>
+            <div className={styles.dropDownMenuTriangle}></div>
+            <div className={styles.profileDropDownMenu}>
+              <nav>
+                <ul>
+                  <li>
+                    <FontAwesomeIcon icon={faUser}></FontAwesomeIcon>
+                    Profile
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faFileLines} />
+                    Applications
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faBookmark} />
+                    Saved Jobs
+                  </li>
+                  <li>
+                    <FontAwesomeIcon icon={faRightFromBracket} />
+                    Logout
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <ul className={styles.userNotLoggedIn}>
+          <li>
+            <Link href="/api/auth/login">Login</Link>
+          </li>
+          <li>
+            <Link href="#">Sign Up</Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 }
